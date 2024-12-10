5726d9b47c0786ec37a83bd20e342b1791d76d82
stinejulius <139495667+stinejulius@users.noreply.github.com>
2024-12-10 09:41:02 +0100

    Added links to all buttons

---
+++ javascript/field-input-picker-drop-down.js
@@ -0,0 +1,20 @@
+// Stine Julius
+// Field input picker drop down - mobile (Fagområde vælger)
+
+
+// Check if JavaScript is working in console
+console.log("The site is working");
+
+/**
+ * GLOBAL VARIABLES
+ */
+
+
+/**
+ * EVENT LISTENER |
+ */
+
+
+/**
+ * FUNCTION |
+ */
