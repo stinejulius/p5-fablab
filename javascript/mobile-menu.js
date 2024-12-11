1d5c5b8f26f0f62dc49acfaa29463b859949b74e
camianker <cami.ankerstjerne@gmail.com>
2024-12-11 10:58:04 +0100

    made last changes in mobile-menu

--- javascript/mobile-menu.js
+++ javascript/mobile-menu.js
@@ -3,13 +3,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.getElementById('menu-icon');
     const mobileMenu = document.getElementById('mobile-menu');
+    if (menuIcon) {
+        // Elementet findes
+        console.log('Elementet med id "menuIcon" blev fundet.');
+      } else {
+        // Elementet findes ikke
+        console.log('Elementet med id "menuIcon" blev ikke fundet.');
+      }
 
-    // Toggle menu shown by icon click
+    // Vis menu ved klik
     menuIcon.addEventListener('click', () => {
         mobileMenu.classList.toggle('active');
     });
 
-    // Close the menu when a link is clicked
+    // Luk menu ved klik på link
     const menuLinks = document.querySelectorAll('#mobile-menu li a');
     menuLinks.forEach(link => {
         link.addEventListener('click', () => {
